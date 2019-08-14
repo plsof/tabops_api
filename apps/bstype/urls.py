61340c8354c657d77b23d51b4ce0c9b7c4be4d4c
pdd <254995740@qq.com>
2019-08-14 13:18:28 +0800

    views命名规范

--- apps/bstype/urls.py
+++ apps/bstype/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, re_path
 
 from .views import BussinessList, ServiceList
-from .views import BussinessViewsets, ServiceViewsets
+from .views import BussinessViewSet, ServiceViewSet
 
 
 urlpatterns = [
     path(r'bussinessall/', BussinessList.as_view()),
-    path(r'bussiness/', BussinessViewsets.as_view({"get": "list", "post": "create"})),
-    re_path(r'^bussiness/(?P<pk>[0-9]+)$', BussinessViewsets.as_view({"put": "update", "patch": "partial_update", "delete": "destroy"})),
+    path(r'bussiness/', BussinessViewSet.as_view({"get": "list", "post": "create"})),
+    re_path(r'^bussiness/(?P<pk>[0-9]+)$', BussinessViewSet.as_view({"put": "update", "patch": "partial_update", "delete": "destroy"})),
     path(r'serviceall/', ServiceList.as_view()),
-    path(r'service/', ServiceViewsets.as_view({"get": "list", "post": "create"})),
-    re_path(r'^service/(?P<pk>[0-9]+)$', ServiceViewsets.as_view({"put": "update", "patch": "partial_update", "delete": "destroy"})),
+    path(r'service/', ServiceViewSet.as_view({"get": "list", "post": "create"})),
+    re_path(r'^service/(?P<pk>[0-9]+)$', ServiceViewSet.as_view({"put": "update", "patch": "partial_update", "delete": "destroy"})),
 ]
