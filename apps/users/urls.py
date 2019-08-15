61c4f9a428e4fd02e03c95c6f1677643cb5d8817
pdd <254995740@qq.com>
2019-08-15 15:25:09 +0800

    重写modelviewset update方法，给账号加密

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,5 +5,6 @@ from .views import UserViewSet
 urlpatterns = [
     path(r'register/', UserViewSet.as_view({"post": "create"})),
     path(r'info/', UserViewSet.as_view({"get": "list"})),
-    re_path(r'^info/(?P<pk>[0-9]+)$', UserViewSet.as_view({"put": "update", "patch": "partial_update", "delete": "destroy"})),
+    # re_path(r'^update/(?P<pk>[0-9]+)$', UserViewSet.as_view({"put": "update"})),
+    re_path(r'^info/(?P<pk>[0-9]+)$', UserViewSet.as_view({"get": "retrieve", "patch": "partial_update", "delete": "destroy"})),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 from common.views import ResponseModelViewSet
 from django.contrib.auth.models import User
-# from rest_framework import status
+from rest_framework import status
 
 from .serializers import UserSerializer
 
@@ -22,6 +22,17 @@ class UserViewSet(ResponseModelViewSet):
         self.perform_create(serializer)
         self.response_format["data"] = serializer.data
         self.response_format["code"] = 0
-        # headers = self.get_success_headers(serializer.data)
-        # return Response(self.response_format, status=status.HTTP_201_CREATED, headers=headers)
-        return Response(self.response_format)
+        headers = self.get_success_headers(serializer.data)
+        return Response(self.response_format, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data['password'] = make_password(serializer.validated_data['password'])
+        self.perform_update(serializer)
+        self.response_format["data"] = serializer.data
+        self.response_format["code"] = 0
+        headers = self.get_success_headers(serializer.data)
+        return Response(self.response_format, status=status.HTTP_206_PARTIAL_CONTENT, headers=headers)
