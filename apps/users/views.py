b9fbaace8be38a73dbdb0c8211e076f89da22c13
pdd <254995740@qq.com>
2019-08-21 09:58:57 +0800

    usr app添加根据username进行查询

--- apps/users/views.py
+++ apps/users/views.py
@@ -13,6 +13,9 @@ class UserViewSet(ResponseModelViewSet):
 
     def get_queryset(self):
         queryset = User.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None and username is not '':
+            queryset = queryset.filter(username=username)
         return queryset
 
     def create(self, request, *args, **kwargs):
