c7fcf267c8974d38963b1effe7660f5660740282
pdd <254995740@qq.com>
2019-10-21 14:54:54 +0800

    architecture import优化

--- apps/architecture/serializer/architecture.py
+++ apps/architecture/serializer/architecture.py
@@ -1,154 +1,135 @@
 from .base import BaseSerializer
-
-from ..models import Wtv
-from ..models import BImsBoot
-from ..models import BImsPanel
-from ..models import Tms
-from ..models import Epg
-from ..models import Search
-from ..models import Pic
-from ..models import Ppl
-from ..models import CosEpg
-from ..models import Uic
-from ..models import MScreen
-from ..models import DMS2
-from ..models import XMpp
-from ..models import NDms
-from ..models import TOS
-from ..models import UCS
-from ..models import MGS
-from ..models import NMC
-from ..models import UBS
+from architecture import models
 
 
 class WtvSerializer(BaseSerializer):
 
     class Meta:
-        model = Wtv
+        model = models.Wtv
         fields = '__all__'
 
 
 class BImsBootSerializer(BaseSerializer):
 
     class Meta:
-        model = BImsBoot
+        model = models.BImsBoot
         fields = '__all__'
 
 
 class BImsPanelSerializer(BaseSerializer):
 
     class Meta:
-        model = BImsPanel
+        model = models.BImsPanel
         fields = '__all__'
 
 
 class TmsSerializer(BaseSerializer):
 
     class Meta:
-        model = Tms
+        model = models.Tms
         fields = '__all__'
 
 
 class EpgSerializer(BaseSerializer):
 
     class Meta:
-        model = Epg
+        model = models.Epg
         fields = '__all__'
 
 
 class SearchSerializer(BaseSerializer):
 
     class Meta:
-        model = Search
+        model = models.Search
         fields = '__all__'
 
 
 class PicSerializer(BaseSerializer):
 
     class Meta:
-        model = Pic
+        model = models.Pic
         fields = '__all__'
 
 
 class PplSerializer(BaseSerializer):
 
     class Meta:
-        model = Ppl
+        model = models.Ppl
         fields = '__all__'
 
 
 class CosEpgSerializer(BaseSerializer):
 
     class Meta:
-        model = CosEpg
+        model = models.CosEpg
         fields = '__all__'
 
 
 class UicSerializer(BaseSerializer):
 
     class Meta:
-        model = Uic
+        model = models.Uic
         fields = '__all__'
 
 
 class MScreenSerializer(BaseSerializer):
 
     class Meta:
-        model = MScreen
+        model = models.MScreen
         fields = '__all__'
 
 
 class DMS2Serializer(BaseSerializer):
 
     class Meta:
-        model = DMS2
+        model = models.DMS2
         fields = '__all__'
 
 
 class XMppSerializer(BaseSerializer):
 
     class Meta:
-        model = XMpp
+        model = models.XMpp
         fields = '__all__'
 
 
 class NDmsSerializer(BaseSerializer):
 
     class Meta:
-        model = NDms
+        model = models.NDms
         fields = '__all__'
 
 
 class TOSSerializer(BaseSerializer):
 
     class Meta:
-        model = TOS
+        model = models.TOS
         fields = '__all__'
 
 
 class UCSSerializer(BaseSerializer):
 
     class Meta:
-        model = UCS
+        model = models.UCS
         fields = '__all__'
 
 
 class MGSSerializer(BaseSerializer):
 
     class Meta:
-        model = MGS
+        model = models.MGS
         fields = '__all__'
 
 
 class NMCSerializer(BaseSerializer):
 
     class Meta:
-        model = NMC
+        model = models.NMC
         fields = '__all__'
 
 
 class UBSSerializer(BaseSerializer):
 
     class Meta:
-        model = UBS
+        model = models.UBS
         fields = '__all__'
