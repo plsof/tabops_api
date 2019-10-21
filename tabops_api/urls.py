7d6938df613a0508d1001b9b0ab5c89acf82a1f1
pdd <254995740@qq.com>
2019-10-21 11:36:00 +0800

    添加文件上传静态目录的访问url

--- tabops_api/urls.py
+++ tabops_api/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -23,5 +25,8 @@ urlpatterns = [
     path(r'api/asset/', include('asset.urls')),
     path(r'api/architecture/', include('architecture.urls')),
     path(r'api/salt/', include('salt.urls')),
-    path(r'api/zabbix/', include('zabbix.urls'))
+    path(r'api/zabbix/', include('zabbix.urls')),
+    path(r'api/upload/', include('upload.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
